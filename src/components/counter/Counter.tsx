4dff8cded23a2cc970f0ed576665cd4a678dfa1d
Roi Dopazo <roidopazo@gmail.com>
2021-11-30 16:37:10 +0100

    [Exercise 1] Added counter into the app

---
+++ src/components/counter/Counter.tsx
@@ -0,0 +1,32 @@
+import { Button, Typography } from '@mui/material';
+import React, { useState } from 'react';
+import useStyles from './counter-styles';
+
+const Counter = () => {
+  const [count, setCount] = useState(0);
+  const classes = useStyles();
+
+  const onDecrementCount = () => {
+    if (count > 0) {
+      setCount(count - 1);
+    }
+  };
+
+  const onIncrementCount = () => {
+    setCount(count + 1);
+  };
+
+  return (
+    <div className={classes.container}>
+      <Button className={classes.button} onClick={onDecrementCount} disabled={count === 0}>
+        -
+      </Button>
+      <Typography>{count}</Typography>
+      <Button className={classes.button} onClick={onIncrementCount}>
+        +
+      </Button>
+    </div>
+  );
+};
+
+export default Counter;
--- src/screens/products/Products.tsx
+++ src/screens/products/Products.tsx
@@ -1,17 +1,25 @@
 import React from 'react';
 import { Typography, Button } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
+import Counter from '../../components/counter/Counter';
+import useStyles from './products-styles';
 
 const Products = () => {
   const navigate = useNavigate();
+  const classes = useStyles();
 
   const onClickButton = () => {
     navigate('details');
   };
 
   return (
-    <div>
-      <Typography>Soy la página de productos</Typography>
+    <div className={classes.container}>
+      <div className={classes.header}>
+        <Typography variant="h2">Página de productos</Typography>
+      </div>
+      <div className={classes.counterContainer}>
+        <Counter />
+      </div>
       <Button onClick={onClickButton}>Ve a la página de detalles</Button>
     </div>
   );
